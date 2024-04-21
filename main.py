da1c643c7bac0bedbae5900e238f7a2fa7482a08
Martin Gogov <mgogov@gmail.com>
2024-04-21 13:43:07 +0300

    indexing

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import util
-import openai
+import json
 
-openai.api_key = util.get_openai_api_key()
+from llama_index import Document
 
 doc = util.summaries_doc()
-
-print(doc.text)
+node_parser = util.get_node_parser(doc.text)
+llm = util.get_openai_llm()
+nodes = node_parser.get_nodes_from_documents([Document(text=doc.text)])
+context = util.get_service_context(llm, node_parser)
+index = util.get_index(doc, context)
--- util.py
+++ util.py
@@ -1,13 +1,27 @@
 import os
 import openai
 import glob
+import logging
+
 from dotenv import load_dotenv, find_dotenv
 from llama_index import Document
+from llama_index.node_parser import SentenceWindowNodeParser
+from llama_index import VectorStoreIndex, StorageContext, load_index_from_storage
+from llama_index import ServiceContext
+from llama_index.llms import OpenAI
+
+logging.basicConfig(level=logging.INFO)
 
 def get_openai_api_key():
     _ = load_dotenv(find_dotenv())
 
     return os.getenv("OPENAI_API_KEY")
+
+def get_openai_llm():
+    openai.api_key = get_openai_api_key()
+    llm = OpenAI(model="gpt-3.5-turbo", temperature=0.1)
+
+    return llm
 
 from llama_index import SimpleDirectoryReader
 
@@ -20,3 +34,45 @@ def summaries_doc():
     full_doc = Document(text="\n\n".join([doc.text for doc in docs]))
 
     return full_doc
+
+def get_node_parser(text):
+    # create the sentence window node parser w/ default settings
+    node_parser = SentenceWindowNodeParser.from_defaults(
+        window_size=3,
+        window_metadata_key="window",
+        original_text_metadata_key="original_text",
+    )
+
+    return node_parser
+
+def get_service_context(llm, node_parser):
+    context = ServiceContext.from_defaults(
+        llm=llm,
+        embed_model="local:BAAI/bge-small-en-v1.5",
+        node_parser=node_parser,
+    )
+
+    return context
+
+def get_index(doc, service_context, reindex=False):
+    if reindex or not os.path.exists("./index_storage"):
+        logging.info("Reindexing...")
+
+        index = VectorStoreIndex.from_documents(
+            [doc], service_context=service_context
+        )
+
+        index.storage_context.persist(persist_dir="./index_storage")
+
+        logging.info("Index created and saved.")
+    else:
+        logging.info("Loading index from storage...")
+
+        index = load_index_from_storage(
+            StorageContext.from_defaults(persist_dir="./index_storage"),
+            service_context=service_context
+        )
+
+        logging.info("Index loaded.")
+
+    return index
