7caa266067309b98c1083be85e2333393756a2e5
Martin Gogov <mgogov@gmail.com>
2024-04-22 18:22:42 +0300

    wip

---
+++ openpod.py
@@ -0,0 +1,70 @@
+import util
+import json
+import os
+import argparse
+import logging
+
+from llama_index import Document
+import llama_index.core
+
+# set debug level to debug
+logging.basicConfig(level=logging.DEBUG)
+
+def setup_rag():
+    doc = util.summaries_doc()
+    node_parser = util.get_node_parser(doc.text)
+    llm = util.get_openai_llm()
+    nodes = node_parser.get_nodes_from_documents([Document(text=doc.text)])
+    context = util.get_service_context(llm, node_parser)
+    index = util.get_index(doc, context)
+    query_engine = util.get_query_engine(index)
+
+    return query_engine
+
+os.environ["TOKENIZERS_PARALLELISM"] = "true"
+
+def chat():
+    query_engine = setup_rag()
+
+    # loop over questions from stdin and answer them
+    while True:
+        question = input("Ask a question: ")
+        result = query_engine.query(question)
+        print(result)
+
+        # break if the user types "exit"
+        if question == "exit":
+            break
+
+        # break if the user presses ctrl+c
+        try:
+            pass
+        except KeyboardInterrupt:
+            break
+
+def setup_argparser():
+    parser = argparse.ArgumentParser(prog='openpod', description='OpenPod command line interface.')
+    # parser.add_argument('--help', action='help', help='Show this help message')
+    parser.add_argument('--eval', action='store_true', help='Evaluate how well the LLM is answering the benchmark questions')
+    parser.add_argument('--chat', action='store_true', help='Chat with the podcast')
+    
+    return parser
+
+def main():
+    parser = setup_argparser()
+    args = parser.parse_args()
+
+    # if no arguments are passed, show the help message
+    if not any(vars(args).values()):
+        parser.print_help()
+        parser.exit()
+
+    if args.chat:
+        chat()
+
+    if args.eval:
+        # TODO implement
+        pass
+
+if __name__ == "__main__":
+    main()
